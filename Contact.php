d58ec901e5933193c77a70f48def7b17a76ee125
Gobinda Ghimire <gowingg1@gmail.com>
2025-01-03 13:20:34 -0800

    imp note: one imp link of the bootstrap which help to expand the nav bar was placed in the footer but i don't used the footer in the contact page so when i add that link <script src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js></script> it works navbar expand and adapt the mobile and tablet screen

--- Contact.php
+++ Contact.php
@@ -7,7 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> CONTACT</title>
 
-  <?php require('links.php');
+  <?php 
+  require('links.php');
   if (!isset($_SESSION["user_id"])) {
    
     // Redirect to login page if session is not set
@@ -291,6 +292,7 @@
     }
     ?>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
   </body>
 
